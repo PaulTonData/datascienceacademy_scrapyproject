994b37bd2522dd6bf8b43f3d844b3432b19fd16a
= <paul.ton@outlook.com>
2017-10-19 18:32:48 -0400

    update items definition, fill out spider

--- veggie_scraper/veggie_scraper/items.py
+++ veggie_scraper/veggie_scraper/items.py
@@ -8,21 +8,19 @@
 from scrapy import Item, Field
 
 class PostItem(Item):
-    forum_id = Field()
     thread_id = Field()
     user_id = Field()
     post_id = Field()
+    post_num = Field()
     text = Field()
     datetime = Field()
 
 class ThreadItem(Item):
-    thread_id = Field()
     url = Field()
     title = Field()
+    thread_id = Field()
+    forum = Field()
 
 class UserItem(Item):
     user_id = Field()
-    name = Field()
-    join_date = Field()
-    post_ct = Field()
-    location = Field()
+    user_url = Field()
--- veggie_scraper/veggie_scraper/spiders/veggie_spider.py
+++ veggie_scraper/veggie_scraper/spiders/veggie_spider.py
@@ -16,23 +16,76 @@ class VeggieSpider(Spider):
         last_page = last_link[last_link.find('index')+5:]
         last_page = last_page[:last_page.find('.')]
 
-        post_list = response.xpath('//a[@class="thread_title_link"]/@href').extract()
+        yield self.parse_page(self, response)
 
-        #filter out any sponsored redirect links
-        post_list = [link for link in post_list if "misc.php" not in link]
-
-        for i in range(2, last_page+1):
+        for i in range(2, int(last_page) + 1):
             yield Request(base_link + "index" + str(i) + ".html", callback=self.parse_page)
-
-        for post in post_list:
-            yield Request(post, callback=self.parse_post)
 
     def parse_page(self, response):
         post_list = response.xpath('//a[@class="thread_title_link"]/@href').extract()
+
+        #filter out any sponsored redirect links     
         post_list = [link for link in post_list if "misc.php" not in link]
 
+        forum = response.url[34:]
+        forum = forum[:forum.find('/')]
+
         for post in post_list:
+            #build ThreadItem
+            item = ThreadItem()
+            item['url'] = post
+
+            title = post[post.find(forum) + len(forum) + 1:]
+            item['title'] = title
+            item['thread_id'] = title[:title.find('-')]
+            item['forum'] = forum
+
+            #yield ThreadItem
+            yield item
+
+            #go into post and gather comments
             yield Request(post, callback=self.parse_post)
 
     def parse_post(self, response):
-        pass
+        yield self.parse_post_page(self, response)
+
+        #check if multi-page thread, if so, build links and loop
+        base_link = response.url[:-5]
+        last_link = response.xpath('//div[@class="pagenav"]//a[starts-with(@title, "Last")]/@href').extract_first()
+        if last_link:
+            last_page = last_link[len(base_link) + 1:-5]
+            for i in range(2, int(last_page) + 1):
+                yield Request(base_link + "-" + str(i) + ".html", callback=self.parse_post_page)
+
+    def parse_post_page(self, response):
+        base_link = response.url
+        #'http://www.veggieboards.com/forum/11-vegetarian-support-forum/225682-almonds-nuts.html'
+
+        tail = base_link[34:-5]
+        #11-vegetarian-support-forum/225682-almonds-nuts
+
+        thread = tail[tail.find('/') + 1:]
+        #225682-almonds-nuts
+
+        thread_id = thread[:thread.find('-')]
+        #225682
+
+        comments = response.xpath('//section[contains(@id, "post")]')
+
+        for comment in comments:
+            user = comment.xpath('.//a[@class="bigusername"]/text()').extract_first()
+            post_id = comment.xpath('.//div[starts-with(@id, "post_message")]/@id').extract_first()
+            post_id = post_id[13:]
+            post_num = comment.xpath('.//span[@class="post-count"]/a//text()').extract_first()
+            text = comment.xpath('.//div[starts-with(@id, "post_message")]').extract_first()
+            datetime = comment.xpath('.//span[@itemprop="dateCreated"]/text()').extract_first()
+
+            item = PostItem()
+            item['thread_id'] = thread_id
+            item['user_id'] = user
+            item['post_id'] = post_id
+            item['post_num'] = post_num
+            item['text'] = text
+            item['datetime'] = datetime
+
+            yield item
